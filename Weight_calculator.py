c3fa94bc6ec8df690a2723c00034aab7ccad2230
Gokul <renukadevi6116s@gmail.com>
2025-07-21 15:29:40 +0530

    Delete Weight_calculator.py

--- Weight_calculator.py
+++
@@ -1,8 +0,0 @@
-weight = int(input("Enter weight: "))
-units = input("kg(k) or lb(l)")
-if units.upper() == 'K':
-  converter = weight*2.205
-  print(f" your weight is {converter} in pounds")
-else:
-  converter = weight/2.205
-  print(f" your weight is {converter} in kilograms.")
